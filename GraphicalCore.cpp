d851a4f31165e93841cc67bd17b08724a53ced26
tobast <tobast@tobast.fr>
2013-04-12 18:58:39 +0200

    Made the base window

---
+++ GraphicalCore.cpp
@@ -0,0 +1,61 @@
+
+/*
+ * SOFTWARE:
+ *   QGiac
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   QGiac is an interface to the command-line CAS software Giac.
+ *
+ * CREDITS:
+ *   All credits goes to Bernard PARISSE <bernard.parisse@ujf-grenoble.fr> for Giac, a library which does all the CAS stuff.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2013 BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#include "GraphicalCore.h"
+
+GraphicalCore::GraphicalCore(QWidget* parent) : QMainWindow(parent)
+{
+	buildWidget();
+}
+
+void GraphicalCore::buildWidget()
+{
+	buildCentralWidget();
+}
+
+void GraphicalCore::buildCentralWidget()
+{
+	centralWidget = new QWidget;
+	l_main = new QVBoxLayout;
+	
+	sessions = new QTabWidget;
+	l_main->addWidget(sessions);
+
+	centralWidget->setLayout(l_main);
+	setCentralWidget(centralWidget);
+}
+
---
+++ GraphicalCore.h
@@ -0,0 +1,61 @@
+
+/*
+ * SOFTWARE:
+ *   QGiac
+ * 
+ * AUTHOR:
+ *   Theophile BASTIAN "Tobast" <contact@tobast.fr>
+ *
+ * WEBSITE:
+ *   http://tobast.fr/
+ *
+ * LICENCE:
+ *   GNU GPL v3
+ *
+ * DESCRIPTION:
+ *   QGiac is an interface to the command-line CAS software Giac.
+ *
+ * CREDITS:
+ *   All credits goes to Bernard PARISSE <bernard.parisse@ujf-grenoble.fr> for Giac, a library which does all the CAS stuff.
+ *
+ * LICENCE HEADER:
+ *   Copyright (C) 2013 BASTIAN Theophile
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+#ifndef DEF_GRAPHICALCORE
+#define DEF_GRAPHICALCORE
+
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QVBoxLayout>
+
+class GraphicalCore : public QMainWindow
+{
+	public:
+		GraphicalCore(QWidget* parent=0);
+
+	private: //meth
+		void buildWidget();
+		void buildCentralWidget();
+
+	private:
+		QWidget* centralWidget;
+		QVBoxLayout* l_main;
+		QTabWidget* sessions;
+};
+
+#endif//DEF_GRAPHICALCORE
+
